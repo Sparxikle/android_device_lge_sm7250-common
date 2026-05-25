fcd31f35a5d2513d5bc4c0fc5cec58040d086175
GyuminLeo <gyuminleo@gmail.com>
2026-05-25 22:18:03 +0900

    Bring up for Android 16
    Change-Id: I7c16f9af63855bf695476fc00201aed730d62724

--- sm7250-common.mk
+++ sm7250-common.mk
@@ -27,8 +27,8 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong
+#PRODUCT_PACKAGES += \
+#    AntHalService-Soong
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+$(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 # Camera
 PRODUCT_PACKAGES += \
