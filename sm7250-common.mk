9547a8365f399f500a276dbbb3ba60ae720f869e
GyuminLeo <gyuminleo@gmail.com>
2026-06-08 13:14:00 +0900

    Added lge vibrator service
    Change-Id: I3fa3b3bd772cdcd8418f398b6ad1a1529b3745a3

--- sm7250-common.mk
+++ sm7250-common.mk
@@ -327,7 +327,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-
+PRODUCT_PACKAGES += \
+		android.hardware.vibrator-service.lge
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
